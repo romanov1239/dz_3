cc1cebca5e4eadfb5f4726ced4a24bb439bef7af
Alex <romanov123@yandex.ru>
2023-04-01 17:29:04 +0300

    ddz

---
+++ zad21/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите x1:");
+double x1 = double.Parse (Console.ReadLine());
+Console.Write("Введите y1:");
+double y1 = double.Parse (Console.ReadLine());
+Console.Write("Введите z1:");
+double z1 = double.Parse (Console.ReadLine());
+Console.Write("Введите x2:");
+double x2 = double.Parse (Console.ReadLine());
+Console.Write("Введите y2:");
+double y2 = double.Parse (Console.ReadLine());
+Console.Write("Введите z2:");
+double z2 = double.Parse (Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow((x1-x2),2) + Math.Pow((y1-y2),2) + Math.Pow((z1-z2),2));
+Console.WriteLine($"d = {d}");
